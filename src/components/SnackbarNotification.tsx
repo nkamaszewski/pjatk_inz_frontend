3285d44153ab320bf71e4c4040c68488b11b0729
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-02 19:22:18 +0100

    dodana mozliwosc multiline snackbar

--- src/components/SnackbarNotification.tsx
+++ src/components/SnackbarNotification.tsx
@@ -1,44 +1,45 @@
-import { Snackbar, SnackbarContent } from '@material-ui/core';
-import styled from 'styled-components';
-import { SnackbarVariant, useSnackbar } from '../providers/NotificationContext';
+import { Snackbar, SnackbarContent } from "@material-ui/core";
+import styled from "styled-components";
+import { SnackbarVariant, useSnackbar } from "../providers/NotificationContext";
 
 const SnackbarMessageStyle = styled.div`
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
+	white-space: pre-line;
 `;
 
 const SnackbarNotification = () => {
-  const { snackbar } = useSnackbar();
+	const { snackbar } = useSnackbar();
 
-  const getSnackbarClass = (variant: SnackbarVariant): string => {
-    switch (variant) {
-      case SnackbarVariant.success:
-        return 'success--background';
-      case SnackbarVariant.error:
-        return 'secondary--background';
-      default:
-        return 'success--background';
-    }
-  };
+	const getSnackbarClass = (variant: SnackbarVariant): string => {
+		switch (variant) {
+			case SnackbarVariant.success:
+				return "success--background";
+			case SnackbarVariant.error:
+				return "secondary--background";
+			default:
+				return "success--background";
+		}
+	};
 
-  return (
-    <Snackbar
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-      open={snackbar.open}
-    >
-      <SnackbarContent
-        className={getSnackbarClass(snackbar.variant)}
-        message={
-          <SnackbarMessageStyle>
-            <p>{snackbar.message}</p>
-          </SnackbarMessageStyle>
-        }
-      />
-    </Snackbar>
-  );
+	return (
+		<Snackbar
+			anchorOrigin={{
+				vertical: "bottom",
+				horizontal: "right",
+			}}
+			open={snackbar.open}
+		>
+			<SnackbarContent
+				className={getSnackbarClass(snackbar.variant)}
+				message={
+					<SnackbarMessageStyle>
+						<p>{snackbar.message}</p>
+					</SnackbarMessageStyle>
+				}
+			/>
+		</Snackbar>
+	);
 };
 
 export default SnackbarNotification;
