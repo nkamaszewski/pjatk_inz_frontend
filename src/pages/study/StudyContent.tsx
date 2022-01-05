0cc7721c649e163d63f4139365bbc2ffc37da1f1
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-05 20:28:12 +0100

    poprawki obsługi błędów study

--- src/pages/study/StudyContent.tsx
+++ src/pages/study/StudyContent.tsx
@@ -1,168 +1,170 @@
-import { Button, TextField } from '@material-ui/core';
-import { useLanguage } from 'providers/LanguageProvider';
-import { useState } from 'react';
-import styled from 'styled-components';
-import { postEducation, updateEducation } from '../../api/Education';
-import { postStudy, updateStudy } from '../../api/Study';
-import GraduateDegreeSelect from '../../components/controls_UI/graduateDegreeSelect/GraduateDegreeSelect';
-import StudyModeSelect from '../../components/controls_UI/StudyModeSelect';
-import UniuversitySelect from '../../components/controls_UI/UniuversitySelect';
+import { Button, TextField } from "@material-ui/core";
+import { useLanguage } from "providers/LanguageProvider";
+import { useState } from "react";
+import styled from "styled-components";
+import { postEducation, updateEducation } from "../../api/Education";
+import { postStudy, updateStudy } from "../../api/Study";
+import GraduateDegreeSelect from "../../components/controls_UI/graduateDegreeSelect/GraduateDegreeSelect";
+import StudyModeSelect from "../../components/controls_UI/StudyModeSelect";
+import UniuversitySelect from "../../components/controls_UI/UniuversitySelect";
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
-} from '../../providers/NotificationContext';
-import { StudiesListDTO } from '../../types/DTO/Study';
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
+} from "../../providers/NotificationContext";
+import { StudiesListDTO } from "../../types/DTO/Study";
+import { useHandleHttpError } from "hooks/useHandleHttpError";
 
 const StudyContentStyle = styled.div`
-  padding: 24px 0;
-  display: grid;
-  grid-row-gap: 16px;
+	padding: 24px 0;
+	display: grid;
+	grid-row-gap: 16px;
 `;
 
 interface Props {
-  closeDrawer: Function;
-  fetchStudies: Function;
-  editStudy?: StudiesListDTO | null;
+	closeDrawer: Function;
+	fetchStudies: Function;
+	editStudy?: StudiesListDTO | null;
 }
 
 const StudyContent = ({ closeDrawer, fetchStudies, editStudy }: Props) => {
-  const [fieldOfStudy, setFieldOfStudy] = useState(
-    editStudy?.FieldOfStudy ?? ''
-  );
-  const [universitetId, setUniversitetId] = useState(
-    editStudy?.IdUniversity ?? ''
-  );
-  const [studyModeId, setStudyModeId] = useState(editStudy?.IdStudyMode ?? '');
-  const [graduateDegreeId, setGraduateDegreeId] = useState(
-    editStudy?.studysGraduateDegree.IdGraduateDegree ?? ''
-  );
-  const [education, setEducation] = useState({
-    Price: editStudy?.studyEducation.Price ?? 0,
-    PriceAccommodation: editStudy?.studyEducation.PriceAccommodation ?? 0,
-    PriceTransit: editStudy?.studyEducation.PriceTransit ?? 0,
-  });
-  const { setSnackbar } = useSnackbar();
+	const [fieldOfStudy, setFieldOfStudy] = useState(
+		editStudy?.FieldOfStudy ?? ""
+	);
+	const [universitetId, setUniversitetId] = useState(
+		editStudy?.IdUniversity ?? ""
+	);
+	const [studyModeId, setStudyModeId] = useState(editStudy?.IdStudyMode ?? "");
+	const [graduateDegreeId, setGraduateDegreeId] = useState(
+		editStudy?.studysGraduateDegree.IdGraduateDegree ?? ""
+	);
+	const [education, setEducation] = useState({
+		Price: editStudy?.studyEducation.Price ?? 0,
+		PriceAccommodation: editStudy?.studyEducation.PriceAccommodation ?? 0,
+		PriceTransit: editStudy?.studyEducation.PriceTransit ?? 0,
+	});
+	const { setSnackbar } = useSnackbar();
+	const handleHttpError = useHandleHttpError();
 
-  const handleOnSave = async () => {
-    try {
-      if (editStudy) {
-        await updateEducation({
-          IdEducation: editStudy.IdEducation,
-          ...education,
-        });
-        await updateStudy({
-          IdEducation: editStudy.IdEducation,
-          FieldOfStudy: fieldOfStudy,
-          IdUniversity: universitetId,
-          IdStudyMode: studyModeId,
-          IdGraduateDegree: graduateDegreeId,
-        });
-        setSnackbar(createSnackbarSuccess('Studia zostały wyedytowane'));
-      } else {
-        const response = await postEducation(education);
-        const { IdEducation } = response.data;
-        await postStudy({
-          IdEducation,
-          FieldOfStudy: fieldOfStudy,
-          IdUniversity: universitetId,
-          IdStudyMode: studyModeId,
-          IdGraduateDegree: graduateDegreeId,
-        });
-        setSnackbar(createSnackbarSuccess('Dodano studia'));
-      }
-      fetchStudies();
-    } catch (e) {
-      setSnackbar(createSnackbarError('Operacja nie powiodła się!'));
-      console.error(e);
-    } finally {
-      closeDrawer();
-    }
-  };
+	const handleOnSave = async () => {
+		try {
+			if (editStudy) {
+				await updateEducation({
+					IdEducation: editStudy.IdEducation,
+					...education,
+				});
+				await updateStudy({
+					IdEducation: editStudy.IdEducation,
+					FieldOfStudy: fieldOfStudy,
+					IdUniversity: universitetId,
+					IdStudyMode: studyModeId,
+					IdGraduateDegree: graduateDegreeId,
+				});
+				setSnackbar(createSnackbarSuccess("Studia zostały wyedytowane"));
+				closeDrawer();
+			} else {
+				const response = await postEducation(education);
+				const { IdEducation } = response.data;
+				await postStudy({
+					IdEducation,
+					FieldOfStudy: fieldOfStudy,
+					IdUniversity: universitetId,
+					IdStudyMode: studyModeId,
+					IdGraduateDegree: graduateDegreeId,
+				});
+				setSnackbar(createSnackbarSuccess("Dodano studia"));
+				closeDrawer();
+			}
+			fetchStudies();
+		} catch (e) {
+			handleHttpError(e);
+			console.error(e);
+		}
+	};
 
-  const handleOnFieldOfStudyChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setFieldOfStudy(event.target.value);
-  };
+	const handleOnFieldOfStudyChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setFieldOfStudy(event.target.value);
+	};
 
-  const handleOnEducationChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setEducation((prevEducation) => ({
-      ...prevEducation,
-      [event.target.name]: event.target.value,
-    }));
-  };
-  const {
-    language: { schema },
-  } = useLanguage();
+	const handleOnEducationChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setEducation((prevEducation) => ({
+			...prevEducation,
+			[event.target.name]: event.target.value,
+		}));
+	};
+	const {
+		language: { schema },
+	} = useLanguage();
 
-  return (
-    <StudyContentStyle>
-      <TextField
-        label={schema.fieldOfStudy}
-        name="fieldOfStudy"
-        type="text"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        value={fieldOfStudy}
-        onChange={handleOnFieldOfStudyChange}
-      />
+	return (
+		<StudyContentStyle>
+			<TextField
+				label={schema.fieldOfStudy}
+				name="fieldOfStudy"
+				type="text"
+				InputLabelProps={{
+					shrink: true,
+				}}
+				value={fieldOfStudy}
+				onChange={handleOnFieldOfStudyChange}
+			/>
 
-      <UniuversitySelect value={universitetId} onChange={setUniversitetId} />
+			<UniuversitySelect value={universitetId} onChange={setUniversitetId} />
 
-      <StudyModeSelect value={studyModeId} onChange={setStudyModeId} />
+			<StudyModeSelect value={studyModeId} onChange={setStudyModeId} />
 
-      <GraduateDegreeSelect
-        value={graduateDegreeId}
-        onChange={setGraduateDegreeId}
-      />
+			<GraduateDegreeSelect
+				value={graduateDegreeId}
+				onChange={setGraduateDegreeId}
+			/>
 
-      <TextField
-        label={schema.price}
-        name="Price"
-        type="number"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        value={education.Price}
-        onChange={handleOnEducationChange}
-      />
+			<TextField
+				label={schema.price}
+				name="Price"
+				type="number"
+				InputLabelProps={{
+					shrink: true,
+				}}
+				value={education.Price}
+				onChange={handleOnEducationChange}
+			/>
 
-      <TextField
-        label={schema.accommodationPrice}
-        name="PriceAccommodation"
-        type="number"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        value={education.PriceAccommodation}
-        onChange={handleOnEducationChange}
-      />
+			<TextField
+				label={schema.accommodationPrice}
+				name="PriceAccommodation"
+				type="number"
+				InputLabelProps={{
+					shrink: true,
+				}}
+				value={education.PriceAccommodation}
+				onChange={handleOnEducationChange}
+			/>
 
-      <TextField
-        label={schema.theCostOfTransport}
-        name="PriceTransit"
-        type="number"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        value={education.PriceTransit}
-        onChange={handleOnEducationChange}
-      />
+			<TextField
+				label={schema.theCostOfTransport}
+				name="PriceTransit"
+				type="number"
+				InputLabelProps={{
+					shrink: true,
+				}}
+				value={education.PriceTransit}
+				onChange={handleOnEducationChange}
+			/>
 
-      <Button
-        disabled={!Boolean(fieldOfStudy)}
-        variant="contained"
-        color="primary"
-        onClick={handleOnSave}
-      >
-        {schema.save}
-      </Button>
-    </StudyContentStyle>
-  );
+			<Button
+				disabled={!Boolean(fieldOfStudy)}
+				variant="contained"
+				color="primary"
+				onClick={handleOnSave}
+			>
+				{schema.save}
+			</Button>
+		</StudyContentStyle>
+	);
 };
 
 export default StudyContent;
--- src/pages/study/StudyList.tsx
+++ src/pages/study/StudyList.tsx
@@ -1,97 +1,99 @@
-import { Drawer } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import { AddParticipation } from 'components/AddParticipation';
-import { ParticipationFieldset } from 'components/participation/ParticipationFieldset';
-import { useDrawer } from 'hooks/useDrawer';
-import { useState } from 'react';
-import styled from 'styled-components';
-import { deleteStudy } from '../../api/Study';
-import DeleteBtn from '../../components/DeleteBtn';
-import EditBtn from '../../components/EditBtn';
+import { Drawer } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import { AddParticipation } from "components/AddParticipation";
+import { ParticipationFieldset } from "components/participation/ParticipationFieldset";
+import { useDrawer } from "hooks/useDrawer";
+import { useState } from "react";
+import styled from "styled-components";
+import { deleteStudy } from "../../api/Study";
+import DeleteBtn from "../../components/DeleteBtn";
+import EditBtn from "../../components/EditBtn";
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
-} from '../../providers/NotificationContext';
-import { StudiesListDTO } from '../../types/DTO/Study';
-import StudyFieldset from './StudyFieldset';
-import StudyListHeader from './StudyListHeader';
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
+} from "../../providers/NotificationContext";
+import { StudiesListDTO } from "../../types/DTO/Study";
+import StudyFieldset from "./StudyFieldset";
+import StudyListHeader from "./StudyListHeader";
+import { useHandleHttpError } from "hooks/useHandleHttpError";
 
 const StudyListStyle = styled.div`
-  padding: 16px;
+	padding: 16px;
 
-  .grid-coach {
-    display: grid;
-    grid-template-columns: 120px 1fr 120px 80px 100px repeat(3, 56px);
-  }
+	.grid-coach {
+		display: grid;
+		grid-template-columns: 120px 1fr 120px 80px 100px repeat(3, 56px);
+	}
 
-  .row {
-    padding: 16px;
-    margin: 4px 0;
-  }
+	.row {
+		padding: 16px;
+		margin: 4px 0;
+	}
 `;
 
 interface Props {
-  studies: StudiesListDTO[];
-  fetchStudies: () => void;
+	studies: StudiesListDTO[];
+	fetchStudies: () => void;
 }
 
 const StudyList = ({ studies, fetchStudies }: Props) => {
-  const [editStudy, setEditStudy]: [StudiesListDTO | null, Function] =
-    useState(null);
-  const { open, openDrawer, closeDrawer } = useDrawer();
-  const [selectedIdEducation, setSelectedIdEducation] = useState('');
-  const { setSnackbar } = useSnackbar();
-  const handleAddParticipation = (id: string) => {
-    setSelectedIdEducation(id);
-    openDrawer();
-  };
-  const handleCloseDrawer = () => setEditStudy(null);
-  const handleDeleteItem = async (id: string) => {
-    try {
-      await deleteStudy(id);
-      fetchStudies();
-      setSnackbar(createSnackbarSuccess('usunięto studia'));
-    } catch (e) {
-      setSnackbar(createSnackbarError('nie udało się usunąć studiów!'));
-    }
-  };
-  return (
-    <StudyListStyle>
-      <Drawer
-        anchor="right"
-        open={Boolean(editStudy)}
-        onClose={handleCloseDrawer}
-      >
-        <StudyFieldset
-          closeDrawer={handleCloseDrawer}
-          fetchStudies={fetchStudies}
-          editStudy={editStudy}
-        />
-      </Drawer>
-      <Drawer anchor="right" open={open} onClose={closeDrawer}>
-        <ParticipationFieldset IdEducation={selectedIdEducation} />
-      </Drawer>
-      <StudyListHeader />
-      {studies.map((study) => {
-        return (
-          <Card key={study.IdEducation} className="grid-coach row">
-            <p>{study.FieldOfStudy}</p>
-            <p>{study.studyUniversity.Name}</p>
-            <p>{study.studyUniversity.City}</p>
-            <p>{study.studysStudyMode.Name}</p>
-            <p>{study.studysGraduateDegree.Name}</p>
+	const [editStudy, setEditStudy]: [StudiesListDTO | null, Function] =
+		useState(null);
+	const { open, openDrawer, closeDrawer } = useDrawer();
+	const [selectedIdEducation, setSelectedIdEducation] = useState("");
+	const { setSnackbar } = useSnackbar();
+	const handleAddParticipation = (id: string) => {
+		setSelectedIdEducation(id);
+		openDrawer();
+	};
+	const handleCloseDrawer = () => setEditStudy(null);
+	const handleHttpError = useHandleHttpError();
+	const handleDeleteItem = async (id: string) => {
+		try {
+			await deleteStudy(id);
+			fetchStudies();
+			setSnackbar(createSnackbarSuccess("usunięto studia"));
+		} catch (e) {
+			handleHttpError(e);
+		}
+	};
+	return (
+		<StudyListStyle>
+			<Drawer
+				anchor="right"
+				open={Boolean(editStudy)}
+				onClose={handleCloseDrawer}
+			>
+				<StudyFieldset
+					closeDrawer={handleCloseDrawer}
+					fetchStudies={fetchStudies}
+					editStudy={editStudy}
+				/>
+			</Drawer>
+			<Drawer anchor="right" open={open} onClose={closeDrawer}>
+				<ParticipationFieldset IdEducation={selectedIdEducation} />
+			</Drawer>
+			<StudyListHeader />
+			{studies.map((study) => {
+				return (
+					<Card key={study.IdEducation} className="grid-coach row">
+						<p>{study.FieldOfStudy}</p>
+						<p>{study.studyUniversity.Name}</p>
+						<p>{study.studyUniversity.City}</p>
+						<p>{study.studysStudyMode.Name}</p>
+						<p>{study.studysGraduateDegree.Name}</p>
 
-            <EditBtn onClick={() => setEditStudy(study)} />
-            <DeleteBtn onClick={() => handleDeleteItem(study.IdEducation)} />
-            <AddParticipation
-              onClick={() => handleAddParticipation(study.IdEducation)}
-            />
-          </Card>
-        );
-      })}
-    </StudyListStyle>
-  );
+						<EditBtn onClick={() => setEditStudy(study)} />
+						<DeleteBtn onClick={() => handleDeleteItem(study.IdEducation)} />
+						<AddParticipation
+							onClick={() => handleAddParticipation(study.IdEducation)}
+						/>
+					</Card>
+				);
+			})}
+		</StudyListStyle>
+	);
 };
 
 export default StudyList;
