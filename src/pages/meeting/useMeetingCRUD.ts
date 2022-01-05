57d807e65fed67af4fdd5ad050282159eb98d586
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-06 00:58:06 +0100

    zmiany w obsłudze błędów meeting

--- src/pages/meeting/useMeetingCRUD.ts
+++ src/pages/meeting/useMeetingCRUD.ts
@@ -1,43 +1,45 @@
 import { deleteMeeting, postMeeting, updateMeeting } from 'api/Meeting';
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
 } from 'providers/NotificationContext';
 import { MeetingDTOShort } from 'types/DTO/Meeting';
+import { useHandleHttpError } from 'hooks/useHandleHttpError';
 
 export const useMeetingCRUD = () => {
-  const { setSnackbar } = useSnackbar();
+	const { setSnackbar } = useSnackbar();
+	const handleHttpError = useHandleHttpError();
 
-  const addItem = async (meeting: Omit<MeetingDTOShort, 'IdMeeting'>) => {
-    try {
-      await postMeeting(meeting);
-      setSnackbar(createSnackbarSuccess('Dodano spotkanie!'));
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Nie udało się dodać spotkania!'));
-    }
-  };
+	const addItem = async (meeting: Omit<MeetingDTOShort, 'IdMeeting'>) => {
+		try {
+			await postMeeting(meeting);
+			setSnackbar(createSnackbarSuccess('Dodano spotkanie!'));
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
 
-  const deleteItem = async (id: string) => {
-    try {
-      await deleteMeeting(id);
-      setSnackbar(createSnackbarSuccess('Usunięto spotkanie!'));
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Nie udało się usunąć spotkania!'));
-    }
-  };
+	const deleteItem = async (id: string) => {
+		try {
+			await deleteMeeting(id);
+			setSnackbar(createSnackbarSuccess('Usunięto spotkanie!'));
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
 
-  const editItem = async (meeting: MeetingDTOShort) => {
-    try {
-      await updateMeeting(meeting);
-      setSnackbar(createSnackbarSuccess('Wyedytowano spotkanie!'));
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Nie udało się wydedytować spotkania!'));
-    }
-  };
+	const editItem = async (meeting: MeetingDTOShort) => {
+		try {
+			await updateMeeting(meeting);
+			setSnackbar(createSnackbarSuccess('Wyedytowano spotkanie!'));
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
 
-  return { addItem, deleteItem, editItem };
+	return { addItem, deleteItem, editItem };
 };
