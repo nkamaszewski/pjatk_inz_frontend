494e3287bbf57ebc84c07661b7c6fa92fecffd4c
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-06 12:38:41 +0100

    zmiana obsł. kom. błędów polls

--- src/pages/polls/PollsList.tsx
+++ src/pages/polls/PollsList.tsx
@@ -6,119 +6,121 @@ import { deleteQuestionnaireOffer } from '../../api/QuestionnaireOffer';
 import DeleteBtn from '../../components/DeleteBtn';
 import EditBtn from '../../components/EditBtn';
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
 } from '../../providers/NotificationContext';
 import { OfferDTO } from '../../types/DTO/Offer';
 import { QuestionnaireOfferDTO } from '../../types/DTO/QuestionnaireOffer';
 import PollsFieldset from './PollsFieldset';
+import { useHandleHttpError } from 'hooks/useHandleHttpError';
 
 const PollsListStyle = styled.div`
-  padding: 24px;
+	padding: 24px;
 
-  .row-content {
-    display: grid;
-    grid-template-columns: 1fr 56px;
-  }
+	.row-content {
+		display: grid;
+		grid-template-columns: 1fr 56px;
+	}
 
-  .subrow {
-    display: grid;
-    grid-template-columns: 240px 100px 1fr 56px 56px;
-    padding: 16px;
-  }
+	.subrow {
+		display: grid;
+		grid-template-columns: 240px 100px 1fr 56px 56px;
+		padding: 16px;
+	}
 
-  .subrow-hover:hover {
-    background-color: rgba(0, 0, 0, 0.04);
-  }
+	.subrow-hover:hover {
+		background-color: rgba(0, 0, 0, 0.04);
+	}
 
-  .subheader {
-    color: rgba(0, 0, 0, 0.56);
-  }
+	.subheader {
+		color: rgba(0, 0, 0, 0.56);
+	}
 `;
 
 interface Props {
-  questionnaireOffers: QuestionnaireOfferDTO[];
-  fetchQuestionnaireOffers: Function;
+	questionnaireOffers: QuestionnaireOfferDTO[];
+	fetchQuestionnaireOffers: Function;
 }
 
 const PollsList = ({
-  questionnaireOffers,
-  fetchQuestionnaireOffers,
+	questionnaireOffers,
+	fetchQuestionnaireOffers,
 }: Props) => {
-  const [editOffer, setEditOffer]: [OfferDTO | null, Function] = useState(null);
-  const { setSnackbar } = useSnackbar();
+	const [editOffer, setEditOffer]: [OfferDTO | null, Function] = useState(null);
+	const { setSnackbar } = useSnackbar();
+	const handleHttpError = useHandleHttpError();
 
-  const handleDeleteItem = async (id: string) => {
-    try {
-      await deleteQuestionnaireOffer(id);
-      setSnackbar(createSnackbarSuccess('Usunięto ankietę!'));
-      fetchQuestionnaireOffers();
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Nie udało się usunąć ankiety!'));
-    }
-  };
+	const handleDeleteItem = async (id: string) => {
+		try {
+			await deleteQuestionnaireOffer(id);
+			setSnackbar(createSnackbarSuccess('Usunięto wniosek!'));
+			fetchQuestionnaireOffers();
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
 
-  const handleDeleteOffer = async (id: string) => {
-    try {
-      await deleteOffer(id);
-      setSnackbar(createSnackbarSuccess('Usunięto ofertę!'));
-      fetchQuestionnaireOffers();
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Nie udało się usunąć oferty!'));
-    }
-  };
+	const handleDeleteOffer = async (id: string) => {
+		try {
+			await deleteOffer(id);
+			setSnackbar(createSnackbarSuccess('Usunięto propozycję szkolenia!'));
+			fetchQuestionnaireOffers();
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
 
-  const handleCloseDrawer = () => setEditOffer(null);
+	const handleCloseDrawer = () => setEditOffer(null);
 
-  return (
-    <PollsListStyle>
-      <Drawer
-        anchor="right"
-        open={Boolean(editOffer)}
-        onClose={handleCloseDrawer}
-      >
-        <PollsFieldset
-          closeDrawer={handleCloseDrawer}
-          fetchQuestionnaireOffers={fetchQuestionnaireOffers}
-          editOffer={editOffer}
-          polls={questionnaireOffers}
-        />
-      </Drawer>
-      {questionnaireOffers.map((questionnaireoffer) => (
-        <div key={questionnaireoffer.IdQuestionnaireOffer}>
-          <div className="row-content">
-            <h3>{questionnaireoffer.Year}</h3>
+	return (
+		<PollsListStyle>
+			<Drawer
+				anchor="right"
+				open={Boolean(editOffer)}
+				onClose={handleCloseDrawer}
+			>
+				<PollsFieldset
+					closeDrawer={handleCloseDrawer}
+					fetchQuestionnaireOffers={fetchQuestionnaireOffers}
+					editOffer={editOffer}
+					polls={questionnaireOffers}
+				/>
+			</Drawer>
+			{questionnaireOffers.map((questionnaireoffer) => (
+				<div key={questionnaireoffer.IdQuestionnaireOffer}>
+					<div className="row-content">
+						<h3>{questionnaireoffer.Year}</h3>
 
-            <DeleteBtn
-              onClick={() =>
-                handleDeleteItem(questionnaireoffer.IdQuestionnaireOffer)
-              }
-            />
-            <div>
-              <header className="subrow subheader">
-                <p>Temat</p>
-                <p>Cena</p>
-                <p>Link</p>
-              </header>
-              {questionnaireoffer.questionnaireOfferOffer.map((offer) => (
-                <div className="subrow subrow-hover">
-                  <p>{offer.Topic}</p>
-                  <p>{offer.Price}</p>
-                  <p>{offer.Link}</p>
-                  <EditBtn onClick={() => setEditOffer(offer)} />
-                  <DeleteBtn onClick={() => handleDeleteOffer(offer.IdOffer)} />
-                </div>
-              ))}
-            </div>
-          </div>
-          <Divider />
-        </div>
-      ))}
-    </PollsListStyle>
-  );
+						<DeleteBtn
+							onClick={() =>
+								handleDeleteItem(questionnaireoffer.IdQuestionnaireOffer)
+							}
+						/>
+						<div>
+							<header className="subrow subheader">
+								<p>Temat</p>
+								<p>Cena</p>
+								<p>Link</p>
+							</header>
+							{questionnaireoffer.questionnaireOfferOffer.map((offer) => (
+								<div className="subrow subrow-hover">
+									<p>{offer.Topic}</p>
+									<p>{offer.Price}</p>
+									<p>{offer.Link}</p>
+									<EditBtn onClick={() => setEditOffer(offer)} />
+									<DeleteBtn onClick={() => handleDeleteOffer(offer.IdOffer)} />
+								</div>
+							))}
+						</div>
+					</div>
+					<Divider />
+				</div>
+			))}
+		</PollsListStyle>
+	);
 };
 
 export default PollsList;
