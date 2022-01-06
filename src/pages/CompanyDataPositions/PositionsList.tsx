2271a74525a9003b4bb03af89b3a05afbebb3d05
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-06 13:11:47 +0100

    zmiana obsł. kom. błędów positions

--- src/pages/CompanyDataPositions/PositionsList.tsx
+++ src/pages/CompanyDataPositions/PositionsList.tsx
@@ -5,70 +5,72 @@ import { deletePosition } from '../../api/Position';
 import DeleteBtn from '../../components/DeleteBtn';
 import EditBtn from '../../components/EditBtn';
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
 } from '../../providers/NotificationContext';
 import { PositionDTO } from '../../types/DTO/Position';
 import PositionFieldset from './PositionFieldset';
+import { useHandleHttpError } from 'hooks/useHandleHttpError';
 
 const PositionsListStyle = styled.div`
-  padding: 24px;
+	padding: 24px;
 
-  .row-content {
-    display: grid;
-    grid-template-columns: 1fr 56px 56px;
-  }
+	.row-content {
+		display: grid;
+		grid-template-columns: 1fr 56px 56px;
+	}
 `;
 
 interface Props {
-  positions: PositionDTO[];
-  fetchPositions: Function;
+	positions: PositionDTO[];
+	fetchPositions: Function;
 }
 
 const PositionsList = ({ positions, fetchPositions }: Props) => {
-  const [editPosition, setEditPosition]: [PositionDTO | null, Function] =
-    useState(null);
-  const { setSnackbar } = useSnackbar();
+	const [editPosition, setEditPosition]: [PositionDTO | null, Function] =
+		useState(null);
+	const { setSnackbar } = useSnackbar();
+	const handleHttpError = useHandleHttpError();
 
-  const handleDeleteItem = async (id: string) => {
-    try {
-      await deletePosition(id);
-      setSnackbar(createSnackbarSuccess('Usunięto stanowisko!'));
-      fetchPositions();
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Nie udało się usunąć stanowiska!'));
-    }
-  };
+	const handleDeleteItem = async (id: string) => {
+		try {
+			await deletePosition(id);
+			setSnackbar(createSnackbarSuccess('Usunięto stanowisko!'));
+			fetchPositions();
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
 
-  const handleCloseDrawer = () => setEditPosition(null);
+	const handleCloseDrawer = () => setEditPosition(null);
 
-  return (
-    <PositionsListStyle>
-      <Drawer
-        anchor="right"
-        open={Boolean(editPosition)}
-        onClose={handleCloseDrawer}
-      >
-        <PositionFieldset
-          closeDrawer={handleCloseDrawer}
-          fetchPositions={fetchPositions}
-          editPosition={editPosition}
-        />
-      </Drawer>
-      {positions.map((position) => (
-        <div key={position.IdPosition}>
-          <div className="row-content">
-            <h3>{position.Name}</h3>
-            <EditBtn onClick={() => setEditPosition(position)} />
-            <DeleteBtn onClick={() => handleDeleteItem(position.IdPosition)} />
-          </div>
-          <Divider />
-        </div>
-      ))}
-    </PositionsListStyle>
-  );
+	return (
+		<PositionsListStyle>
+			<Drawer
+				anchor="right"
+				open={Boolean(editPosition)}
+				onClose={handleCloseDrawer}
+			>
+				<PositionFieldset
+					closeDrawer={handleCloseDrawer}
+					fetchPositions={fetchPositions}
+					editPosition={editPosition}
+				/>
+			</Drawer>
+			{positions.map((position) => (
+				<div key={position.IdPosition}>
+					<div className="row-content">
+						<h3>{position.Name}</h3>
+						<EditBtn onClick={() => setEditPosition(position)} />
+						<DeleteBtn onClick={() => handleDeleteItem(position.IdPosition)} />
+					</div>
+					<Divider />
+				</div>
+			))}
+		</PositionsListStyle>
+	);
 };
 
 export default PositionsList;
