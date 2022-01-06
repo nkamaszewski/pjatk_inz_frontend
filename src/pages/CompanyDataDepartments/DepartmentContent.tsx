26e0e50e04d82b198946879c0af27db1cafc3086
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-06 13:00:34 +0100

    zmieniona obsł. kom. błędów department/division

--- src/pages/CompanyDataDepartments/DepartmentContent.tsx
+++ src/pages/CompanyDataDepartments/DepartmentContent.tsx
@@ -1,109 +1,111 @@
 import {
-  Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
+	Button,
+	FormControl,
+	InputLabel,
+	MenuItem,
+	Select,
+	TextField,
 } from '@material-ui/core';
 import { useLanguage } from 'providers/LanguageProvider';
 import { useState } from 'react';
 import styled from 'styled-components';
 import { postDepartment, updateDepartment } from '../../api/Department';
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
 } from '../../providers/NotificationContext';
 import { DepartmentDTO } from '../../types/DTO/Department';
 import { DivisionDTO } from '../../types/DTO/Division';
+import { useHandleHttpError } from 'hooks/useHandleHttpError';
 
 const DepartmentContentStyle = styled.div`
-  padding: 24px 0;
-  display: grid;
-  grid-row-gap: 16px;
+	padding: 24px 0;
+	display: grid;
+	grid-row-gap: 16px;
 `;
 
 interface Props {
-  divisions: DivisionDTO[];
-  closeDrawer: Function;
-  fetchDivisionsDepartments: Function;
-  editDepartment?: DepartmentDTO | null;
+	divisions: DivisionDTO[];
+	closeDrawer: Function;
+	fetchDivisionsDepartments: Function;
+	editDepartment?: DepartmentDTO | null;
 }
 
 const DepartmentContent = ({
-  divisions,
-  closeDrawer,
-  fetchDivisionsDepartments,
-  editDepartment,
+	divisions,
+	closeDrawer,
+	fetchDivisionsDepartments,
+	editDepartment,
 }: Props) => {
-  const [name, setName] = useState(editDepartment?.Name ?? '');
-  const [selectedDivision, setSelectedDivision] = useState(
-    editDepartment?.IdDivision ?? ''
-  );
-  const { setSnackbar } = useSnackbar();
+	const [name, setName] = useState(editDepartment?.Name ?? '');
+	const [selectedDivision, setSelectedDivision] = useState(
+		editDepartment?.IdDivision ?? ''
+	);
+	const { setSnackbar } = useSnackbar();
+	const handleHttpError = useHandleHttpError();
 
-  const handleOnNameChange = (e: any) => {
-    e.persist();
-    setName(e.target.value);
-  };
+	const handleOnNameChange = (e: any) => {
+		e.persist();
+		setName(e.target.value);
+	};
 
-  const handleSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setSelectedDivision(event.target.value as string);
-  };
+	const handleSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+		setSelectedDivision(event.target.value as string);
+	};
 
-  const handleOnSave = async () => {
-    const newDepartment = { Name: name, IdDivision: selectedDivision };
-    try {
-      if (editDepartment) {
-        await updateDepartment({
-          IdDepartment: editDepartment.IdDepartment,
-          ...newDepartment,
-        });
-        fetchDivisionsDepartments();
-        setSnackbar(createSnackbarSuccess('edytowano wydział'));
-      } else {
-        await postDepartment(newDepartment);
-        fetchDivisionsDepartments();
-        setSnackbar(createSnackbarSuccess('dodano wydział'));
-      }
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Operacja nie powiodła się!'));
-    } finally {
-      closeDrawer();
-    }
-  };
-  const {
-    language: { schema },
-  } = useLanguage();
+	const handleOnSave = async () => {
+		const newDepartment = { Name: name, IdDivision: selectedDivision };
+		try {
+			if (editDepartment) {
+				await updateDepartment({
+					IdDepartment: editDepartment.IdDepartment,
+					...newDepartment,
+				});
+				fetchDivisionsDepartments();
+				setSnackbar(createSnackbarSuccess('edytowano wydział'));
+				closeDrawer();
+			} else {
+				await postDepartment(newDepartment);
+				fetchDivisionsDepartments();
+				setSnackbar(createSnackbarSuccess('dodano wydział'));
+				closeDrawer();
+			}
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
+	const {
+		language: { schema },
+	} = useLanguage();
 
-  return (
-    <DepartmentContentStyle>
-      <TextField
-        fullWidth
-        label={schema.name}
-        value={name}
-        onChange={handleOnNameChange}
-      />
-      <FormControl fullWidth>
-        <InputLabel>{schema.department}</InputLabel>
-        <Select value={selectedDivision} onChange={handleSelectChange}>
-          {divisions.map((division) => (
-            <MenuItem value={division.IdDivision}>{division.Name}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Button
-        disabled={!Boolean(name) && !Boolean(selectedDivision)}
-        variant="contained"
-        color="primary"
-        onClick={handleOnSave}
-      >
-        {schema.save}
-      </Button>
-    </DepartmentContentStyle>
-  );
+	return (
+		<DepartmentContentStyle>
+			<TextField
+				fullWidth
+				label={schema.name}
+				value={name}
+				onChange={handleOnNameChange}
+			/>
+			<FormControl fullWidth>
+				<InputLabel>{schema.department}</InputLabel>
+				<Select value={selectedDivision} onChange={handleSelectChange}>
+					{divisions.map((division) => (
+						<MenuItem value={division.IdDivision}>{division.Name}</MenuItem>
+					))}
+				</Select>
+			</FormControl>
+			<Button
+				disabled={!Boolean(name) && !Boolean(selectedDivision)}
+				variant="contained"
+				color="primary"
+				onClick={handleOnSave}
+			>
+				{schema.save}
+			</Button>
+		</DepartmentContentStyle>
+	);
 };
 
 export default DepartmentContent;
--- src/pages/CompanyDataDepartments/DivisionContent.tsx
+++ src/pages/CompanyDataDepartments/DivisionContent.tsx
@@ -4,81 +4,83 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { postDivision, updateDivision } from '../../api/Division';
 import {
-  createSnackbarError,
-  createSnackbarSuccess,
-  useSnackbar,
+	createSnackbarError,
+	createSnackbarSuccess,
+	useSnackbar,
 } from '../../providers/NotificationContext';
 import { DivisionDTO } from '../../types/DTO/Division';
+import { useHandleHttpError } from 'hooks/useHandleHttpError';
 
 const DivisionContentStyle = styled.div`
-  padding: 24px 0;
-  display: grid;
-  grid-row-gap: 16px;
+	padding: 24px 0;
+	display: grid;
+	grid-row-gap: 16px;
 `;
 
 interface Props {
-  closeDrawer: Function;
-  fetchDivisionsDepartments: Function;
-  editDivision?: DivisionDTO | null;
+	closeDrawer: Function;
+	fetchDivisionsDepartments: Function;
+	editDivision?: DivisionDTO | null;
 }
 
 const DivisionContent = ({
-  closeDrawer,
-  fetchDivisionsDepartments,
-  editDivision,
+	closeDrawer,
+	fetchDivisionsDepartments,
+	editDivision,
 }: Props) => {
-  const [name, setName] = useState(editDivision?.Name ?? '');
-  const { setSnackbar } = useSnackbar();
+	const [name, setName] = useState(editDivision?.Name ?? '');
+	const { setSnackbar } = useSnackbar();
+	const handleHttpError = useHandleHttpError();
 
-  const handleOnNameChange = (e: any) => {
-    e.persist();
-    setName(e.target.value);
-  };
+	const handleOnNameChange = (e: any) => {
+		e.persist();
+		setName(e.target.value);
+	};
 
-  const handleOnSave = async () => {
-    const newDivision = { Name: name };
-    try {
-      if (editDivision) {
-        await updateDivision({
-          ...newDivision,
-          IdDivision: editDivision.IdDivision,
-        });
-        fetchDivisionsDepartments();
-        setSnackbar(createSnackbarSuccess('edytowano pion'));
-      } else {
-        await postDivision(newDivision);
-        fetchDivisionsDepartments();
-        setSnackbar(createSnackbarSuccess('dodano pion'));
-      }
-    } catch (e) {
-      console.error(e);
-      setSnackbar(createSnackbarError('Operacja nie powiodła się!'));
-    } finally {
-      closeDrawer();
-    }
-  };
-  const {
-    language: { schema },
-  } = useLanguage();
+	const handleOnSave = async () => {
+		const newDivision = { Name: name };
+		try {
+			if (editDivision) {
+				await updateDivision({
+					...newDivision,
+					IdDivision: editDivision.IdDivision,
+				});
+				fetchDivisionsDepartments();
+				setSnackbar(createSnackbarSuccess('edytowano pion'));
+				closeDrawer();
+			} else {
+				await postDivision(newDivision);
+				fetchDivisionsDepartments();
+				setSnackbar(createSnackbarSuccess('dodano pion'));
+				closeDrawer();
+			}
+		} catch (e) {
+			console.error(e);
+			handleHttpError(e);
+		}
+	};
+	const {
+		language: { schema },
+	} = useLanguage();
 
-  return (
-    <DivisionContentStyle>
-      <TextField
-        fullWidth
-        label={schema.name}
-        value={name}
-        onChange={handleOnNameChange}
-      />
-      <Button
-        disabled={!Boolean(name)}
-        variant="contained"
-        color="primary"
-        onClick={handleOnSave}
-      >
-        {schema.save}
-      </Button>
-    </DivisionContentStyle>
-  );
+	return (
+		<DivisionContentStyle>
+			<TextField
+				fullWidth
+				label={schema.name}
+				value={name}
+				onChange={handleOnNameChange}
+			/>
+			<Button
+				disabled={!Boolean(name)}
+				variant="contained"
+				color="primary"
+				onClick={handleOnSave}
+			>
+				{schema.save}
+			</Button>
+		</DivisionContentStyle>
+	);
 };
 
 export default DivisionContent;
