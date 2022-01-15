237a18edfa5161aa556d5449cf20a7c05c340681
Krzysztof Zebrowski <s17531@pjwstk.edu.pl>
2022-01-15 15:22:34 +0100

    zmiana nazwy certyfikatu

--- src/components/participation/ParticipationsList.tsx
+++ src/components/participation/ParticipationsList.tsx
@@ -8,54 +8,66 @@ import styled from 'styled-components';
 import { ParticipationsListDTO } from 'types/DTO/Participation';
 
 const ParticipationsListStyled = styled.div`
-  .grid-participation {
-    padding: 16px;
-    margin: 4px 0;
-    display: grid;
-    grid-template-columns: 200px 1fr 56px 56px;
-  }
+	.grid-participation {
+		padding: 16px;
+		margin: 4px 0;
+		display: grid;
+		grid-template-columns: 200px 1fr 56px 56px;
+	}
 `;
 
 interface ParticipationsListProps {
-  participations: ParticipationsListDTO[];
+	participations: ParticipationsListDTO[];
 }
 
 export const ParticipationsList = ({
-  participations,
+	participations,
 }: ParticipationsListProps) => {
-  const deleteMutation = useDeleteParticipationMutation();
-  const schema = useLanguageSchema();
+	const deleteMutation = useDeleteParticipationMutation();
+	const schema = useLanguageSchema();
 
-  const downloadFile = async (id: string) => {
-    const res = await getPaticipateCeritification(id);
-    const url = window.URL.createObjectURL(new Blob([res.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'zal.pdf');
-    document.body.appendChild(link);
-    link.click();
-  };
+	const downloadFile = async (
+		id: string,
+		lastName: string,
+		firstName: string
+	) => {
+		const res = await getPaticipateCeritification(id);
+		const url = window.URL.createObjectURL(new Blob([res.data]));
+		const link = document.createElement('a');
+		link.href = url;
+		link.setAttribute('download', `certyfikat_${lastName}_${firstName}.pdf`);
+		document.body.appendChild(link);
+		link.click();
+	};
 
-  return (
-    <ParticipationsListStyled>
-      <header className="grid-participation">
-        <p>{schema.firstName}</p>
-        <p>{schema.lastName}</p>
-      </header>
-      {participations.map((part) => (
-        <Card key={part.IdEducation} className="grid-participation">
-          <p>{part.participationEmployee.employeePerson.FirstName}</p>
-          <p>{part.participationEmployee.employeePerson.LastName}</p>
-          {part.CertificateOfCompletion ? (
-            <DownloadBtn onClick={() => downloadFile(part.IdParticipation)} />
-          ) : (
-            <p />
-          )}
-          <DeleteBtn
-            onClick={() => deleteMutation.mutate({ id: part.IdParticipation })}
-          />
-        </Card>
-      ))}
-    </ParticipationsListStyled>
-  );
+	return (
+		<ParticipationsListStyled>
+			<header className="grid-participation">
+				<p>{schema.firstName}</p>
+				<p>{schema.lastName}</p>
+			</header>
+			{participations.map((part) => (
+				<Card key={part.IdEducation} className="grid-participation">
+					<p>{part.participationEmployee.employeePerson.FirstName}</p>
+					<p>{part.participationEmployee.employeePerson.LastName}</p>
+					{part.CertificateOfCompletion ? (
+						<DownloadBtn
+							onClick={() =>
+								downloadFile(
+									part.IdParticipation,
+									part.participationEmployee.employeePerson.LastName,
+									part.participationEmployee.employeePerson.FirstName
+								)
+							}
+						/>
+					) : (
+						<p />
+					)}
+					<DeleteBtn
+						onClick={() => deleteMutation.mutate({ id: part.IdParticipation })}
+					/>
+				</Card>
+			))}
+		</ParticipationsListStyled>
+	);
 };
